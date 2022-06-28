e7b982025da8d965b46336db4fa757711fbf0228
Cristhian Mesta <cristhianmesta@hotmail.com>
2022-06-27 20:16:56 -0500

    Se agregaron Rextie y Kambista

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ import requests
 from sqlserver import *
 
 
+SERVICIOS = (
+    { 
+        "PORTAL"    : "KAMBISTA", 
+        "URL"       : "https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&amount=8&active=R",
+        "METODO"    : "GET"
+    },
+    { 
+        "PORTAL"    : "REXTIE", 
+        "URL"       : "https://app.rextie.com/api/v1/fxrates/rate/?origin=template-original&utm_source=search,adwords&utm_medium=cpc,ppc&utm_id=brand&utm_term=brand,rextie&utm_content=performance&utm_campaign=performance_aon_brand_registros_search&hsa_acc=5383902905&hsa_cam=14396964907&hsa_grp=125801717509&hsa_ad=541122542085&hsa_src=g&hsa_tgt=kwd-321924773704&hsa_kw=rextie&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gclid=cjwkcajwquwvbhbreiwat1kmwi5_rmmlhvedqxfofgsscudi9ca-loipezxlzih6o4olwwed_3tsmroc88eqavd_bwe",
+        "METODO"    : "POST"
+    },
+    { 
+        "PORTAL"    : "TKAMBIO", 
+        "URL"       : "https://tkambio.com/wp-admin/admin-ajax.php?action=get_exchange_rate",
+        "METODO"    : "GET"
+    }
+)
+
 SQL_SERVER  = 'CM3-THINKPAD' 
 SQL_DATABASE = 'TIPO_CAMBIO' 
 SQL_USERNAME = '' 
@@ -15,49 +33,60 @@ sql_cnn_str = (
         r'DATABASE='+SQL_DATABASE+';'
         r'Trusted_Connection=yes;'
 )
+precio_compra_anterior  = {"KAMBISTA": 0, "REXTIE": 0, "TKAMBIO": 0}
+precio_compra           = 0
 
-precio_venta_anterior   = 0
+precio_venta_anterior   = {"KAMBISTA": 0, "REXTIE": 0, "TKAMBIO": 0}
 precio_venta            = 0
 
 while True:
-    resp = requests.get('https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&amount=8&active=R')
-    moment = datetime.now()
-    
-    if resp.status_code==200:
 
-        try:
-            data = resp.json()
-            
-            precio_compra = data['tc']['bid']
-            precio_venta  = data['tc']['ask']
-            
+    for servicio in SERVICIOS:
 
-            icon = "🟡"
+        resp = requests.request(servicio["METODO"],servicio["URL"])
+        moment = datetime.now()
+        
+        if resp.status_code==200 or resp.status_code==201:
 
-            if precio_venta > precio_venta_anterior and precio_venta_anterior != 0: icon = "🔴"
-            if precio_venta < precio_venta_anterior and precio_venta_anterior != 0: icon = "🟢"
+            try:
+                data = resp.json()
 
-            precio_venta_anterior = precio_venta
+                if servicio["PORTAL"] == "KAMBISTA":
+                    precio_compra = float(data['tc']['bid'])
+                    precio_venta  = float(data['tc']['ask'])
+                
+                if servicio["PORTAL"] == "REXTIE":
+                    precio_compra = float(data['fx_rate_buy'])
+                    precio_venta  = float(data['fx_rate_sell'])
+                    
+                if servicio["PORTAL"] == "TKAMBIO":
+                    precio_compra = float(data['buying_rate'])
+                    precio_venta  = float(data['selling_rate'])
+                
+                icon_pc = "🟡"
+                pc_anterior = float(precio_compra_anterior[servicio["PORTAL"]])
+         
+                
+                if precio_compra>pc_anterior and pc_anterior!=0: icon_pc = "🔴"
+                if precio_compra<pc_anterior and pc_anterior!=0: icon_pc = "🟢"
+              
+                precio_compra_anterior[servicio["PORTAL"]] = precio_compra
 
-            print(moment, '\t', precio_compra, '\t', precio_venta, icon)
 
-            db = SqlServer(sql_cnn_str)
-            db.insert('TIPO_CAMBIO_KAMBISTA',{'Fecha' : moment, 'Compra' : precio_compra, 'Venta': precio_venta })
+                icon_pv = "🟡"
+                pv_anterior = float(precio_venta_anterior[servicio["PORTAL"]])
 
-        except:
-            print("No se pudieron recuperar los datos")
+                if precio_venta > pv_anterior and pv_anterior!=0: icon_pv = "🔴"
+                if precio_venta < pv_anterior and pv_anterior!=0: icon_pv = "🟢"
+
+                precio_venta_anterior[servicio["PORTAL"]] = precio_venta
+
+                print(moment,'\t', servicio["PORTAL"],'\t', precio_compra, icon_pc,'\t', precio_venta, icon_pv)
+
+                db = SqlServer(sql_cnn_str)
+                db.insert('TIPO_CAMBIO_'+servicio["PORTAL"],{'Fecha' : moment, 'Compra' : precio_compra, 'Venta': precio_venta })
+
+            except:
+                print("No se pudieron recuperar los datos")
 
     time.sleep(20) 
-
-
-# from bs4 import BeautifulSoup
-# r = requests.get('https://kambista.com/')
-# # soup = BeautifulSoup(r.text, "html.parser")
-# # time.sleep(3)
-# # precio_compra = soup.find("strong", {"id": "valcompra"}).text.strip()
-# # precio_venta = soup.find("strong", {"id": "valventa"}).text.strip()
-# # while True:
-# #     print(precio_compra)
-# #     print(precio_venta)
-# #     print("-------------")
-# #     time.sleep(3) 
